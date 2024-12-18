94c67f8e8add68016cf4776b076ce8e58333c7ca
shujanshaikh <shaikhshujan@gmail.com>
2024-12-18 02:41:33 +0000

    updated code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import Together from 'together-ai';
 import { basePrompt as reactBasePrompt } from "./defaults/react";
 import { basePrompt as nodeBasePrompt } from "./defaults/node";
 import { BASE_PROMPT, getSystemPrompt } from "./prompts";
+import { Chat } from "together-ai/resources";
 require('dotenv').config()
 
 const app = express();
@@ -14,35 +15,27 @@ const client = new Together({
 
 
 app.post("/template" ,async (req , res) =>{
-
-
-  let fullResponse = '';
   const prompt = req.body.prompt;
 
   const response = await client.chat.completions.create({
         model: 'meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo',
         messages: [
+          { role: 'user', content: prompt },
           { 
             role: 'system' , 
             content: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra"
           },
-          { role: 'user', content: prompt }
+          
 
         ],
         temperature: 1,
         max_tokens: 200,
-        stream: true,
         });
-
-        for await (const chunk of response) {
-          if (chunk.choices[0]?.delta?.content) {
-              fullResponse += chunk.choices[0].delta.content;
-          }
-      }
-      
-      const answer = fullResponse.toString().toLowerCase();
-
-    if(answer === "react"){
+      //console.log(response.choices[0].message?.content)
+        
+       const answer = response.choices[0].message?.content;
+        console.log(answer);
+    if(answer === 'react'){
       res.json({
         prompt: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
         uiPrompts: [reactBasePrompt]
@@ -50,7 +43,7 @@ app.post("/template" ,async (req , res) =>{
       return;
     }
 
-    if(answer === "node"){
+    if(answer === 'node'){
       res.json({
         prompt: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
         uiPrompts: [nodeBasePrompt]
@@ -67,17 +60,19 @@ app.post("/chat", async(req, res)=> {
 
   let fullResponse = "";
   const messages = req.body.messages;
-  //@ts-ignore
   const response = await client.chat.completions.create({
     model: 'meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo',
-    messages: messages,
-    system: getSystemPrompt(),
+    messages: [{
+      role: "system" ,
+      content: getSystemPrompt(),
+    }, {
+      role: "user" ,
+      content: messages
+    }],
     stream: true,
     temperature: 1,
     max_tokens: 5000,
-    
-   
-  });
+});
   
   for await (const chunk of response) {
     if (chunk.choices[0]?.delta?.content) {
